eecec547252892884bc44c1b2c5f74680c6a1fc3
frostyausty <priceaustin62@gmail.com>
2020-10-09 18:19:21 -0500

    added function to grab data by username

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,3 +9,21 @@ var getUserRepos = function(user) {
         });
     });
 };  
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
